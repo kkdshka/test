53b28307d4e28c53fb626733dc3ea1d067f85ed4
kkdshka <kkdshka@gmail.com>
2021-10-08 11:39:16 +0300

    Finished repository design

--- src/components/repositories/Repository.tsx
+++ src/components/repositories/Repository.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 import "./Repository.scss";
+import { RiBookMarkLine } from "react-icons/ri";
+import { HiOutlineStar } from "react-icons/hi";
+import { BiGitRepoForked } from "react-icons/bi";
 import { IRepository } from "../../../types/IRepository";
 import { StarButton } from "./StarButton";
 
@@ -11,7 +14,39 @@ export const Repository = ({ repository }: Props) => {
   return (
     <article className="repository-container">
       <StarButton />
-      <div className="repository-name">{`${repository.username} / ${repository.repositoryName}`}</div>
+      <div className="repository-name-container">
+        <RiBookMarkLine className="bookmark-icon" />
+        <a
+          href={repository.url}
+          className={"repository-name"}
+        >{`${repository.username} / ${repository.repositoryName}`}</a>
+      </div>
+      <p className="description">{repository.description}</p>
+      <div className="repository-footer">
+        {repository.language && (
+          <span className="repository-footer-element">
+            {repository.language}
+          </span>
+        )}
+        <span className="repository-footer-element">
+          <HiOutlineStar className={"star-icon"} />
+          {repository.totalStars}
+        </span>
+        <span className="repository-footer-element">
+          <BiGitRepoForked className={"star-icon"} />
+          {repository.totalStars}
+        </span>
+        <span className="repository-footer-element">
+          Built by{" "}
+          {repository.builtBy.map((person) => (
+            <img
+              className="person-avatar"
+              src={person.avatar}
+              alt={person.username}
+            />
+          ))}
+        </span>
+      </div>
     </article>
   );
 };

c09b3787e8061be635e56aa8261ee68bd5f738e1
kkdshka <kkdshka@gmail.com>
2021-10-11 11:07:37 +0300

    Added language filter for developers

--- src/components/developers/Developers.tsx
+++ src/components/developers/Developers.tsx
@@ -5,20 +5,45 @@ import { IDeveloper } from "../../../types/IDeveloper";
 import axios from "axios";
 import { Developer } from "./Developer";
 import { Navigation } from "../common/Navigation";
+import { programmingLanguageOptions } from "../../utils/selectorsOptions";
+import { Selector } from "../common/Selector";
 
 export const Developers = () => {
   const [developers, setDevelopers] = useState<Array<IDeveloper>>([]);
+  const [languageFilter, setLanguageFilter] = useState<string>("");
+  const [loading, setLoading] = useState<boolean>(true);
 
   useEffect(() => {
-    axios
-      .get("https://gh-trending-api.herokuapp.com/developers")
-      .then((result) => {
-        setDevelopers(result.data);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }, []);
+    if (languageFilter !== "") {
+      setLoading(true);
+      axios
+        .get(
+          `https://gh-trending-api.herokuapp.com/developers/${languageFilter}`
+        )
+        .then((result) => {
+          setDevelopers(result.data);
+          setLoading(false);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    } else {
+      setLoading(true);
+      axios
+        .get("https://gh-trending-api.herokuapp.com/developers")
+        .then((result) => {
+          setDevelopers(result.data);
+          setLoading(false);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+  }, [languageFilter, setLanguageFilter]);
+
+  const handleLanguageChange = (language: string) => {
+    setLanguageFilter(language);
+  };
 
   return (
     <div className="flex-container">
@@ -26,10 +51,22 @@ export const Developers = () => {
       <div className="developers-container">
         <div className="developers-header">
           <Navigation activeLink="dev" />
+          <Selector
+            name="Language"
+            options={programmingLanguageOptions}
+            onChange={handleLanguageChange}
+          />
         </div>
-        {developers.map((developer, index) => (
-          <Developer key={`developer-${index}`} developer={developer} />
-        ))}
+        {developers.length > 0 || loading ? (
+          developers.map((developer, index) => (
+            <Developer key={`developer-${index}`} developer={developer} />
+          ))
+        ) : (
+          <div className="no-dev-data">
+            It looks like we don’t have any trending developers for{" "}
+            {languageFilter}.
+          </div>
+        )}
       </div>
     </div>
   );
